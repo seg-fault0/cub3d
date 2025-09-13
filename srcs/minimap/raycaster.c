1837669b3d2b382a9c7a639ef5d8e4ac113fae86
Walid Imam <wimam@e2r9p2.1337.ma>
2025-09-13 08:10:43 +0100

    limit the range of casted rays in minimap

--- srcs/minimap/raycaster.c
+++ srcs/minimap/raycaster.c
@@ -6,7 +6,7 @@
 /*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 13:20:01 by wimam             #+#    #+#             */
-/*   Updated: 2025/09/12 14:10:11 by wimam            ###   ########.fr       */
+/*   Updated: 2025/09/13 08:10:05 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,42 @@
 #define RAY_COLOR 0xFF0000
 #define TILE_SIZE 20
 
+static bool	ray_mm_border(int px, int py, int rx, int ry)
+{
+	int	screen_x;
+	int	screen_y;
+
+	screen_x = (rx - px) * TILE_SIZE + 210;
+	screen_y = (ry - py) * TILE_SIZE + 110;
+	if (screen_x >= 390 || screen_y >= 210)
+		return (true);
+	return (false);
+}
+
 static void	draw_ray(t_cub *cub, float angle)
 {
 	float	rx;
 	float	ry;
 	float	px;
 	float	py;
+	int		i;
 
 	px = cub->player.xp;
 	py = cub->player.yp;
 	rx = px;
 	ry = py;
+	i = 0;
 	while (cub->parse.map[(int)ry][(int)rx] != '1')
 	{
+		if (ray_mm_border(px, py, rx, ry) == true)
+			break ;
 		mlx_pixel_put(cub->mlx.mlx, cub->mlx.win,
 			(rx - px) * TILE_SIZE + 210,
 			(ry - py) * TILE_SIZE + 110,
 			RAY_COLOR);
 		rx += cos(angle) * RAY_STEP;
 		ry += sin(angle) * RAY_STEP;
+		i++;
 	}
 }
 
