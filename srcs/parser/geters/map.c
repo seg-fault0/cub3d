c701502cf1e03dda1cdf383d1d2467145ca1c1a1
Walid Imam <wimam@e2r9p1.1337.ma>
2025-11-02 16:51:04 +0100

    better function to find map i

--- srcs/parser/geters/map.c
+++ srcs/parser/geters/map.c
@@ -3,47 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wimam <walidimam69gmail.com>               +#+  +:+       +#+        */
+/*   By: wimam <walidimam69@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 09:30:36 by wimam             #+#    #+#             */
-/*   Updated: 2025/10/13 11:58:00 by wimam            ###   ########.fr       */
+/*   Updated: 2025/11/02 16:50:31 by wimam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-static int	get_cur_i(char **file)
+static bool	is_top_of_map(char *line)
+{
+	int		i;
+	bool	one_found;
+
+	one_found = false;
+	i = -1;
+	while (line[++i] && line[i] != '\n')
+	{
+		if (line[i] != '1' && line[i] != ' ')
+			return (false);
+		if (line[i] == '1')
+			one_found = true;
+	}
+	if (one_found == true)
+		return (true);
+	return (false);
+}
+
+static int	get_map_i(char **file)
 {
 	int	i;
-	int	last;
 
 	i = -1;
-	last = 0;
 	while (file[++i])
 	{
-		if (ft_memcmp(file[i], "NO", 2) == 0
-			|| ft_memcmp(file[i], "SO", 2) == 0
-			|| ft_memcmp(file[i], "WE", 2) == 0
-			|| ft_memcmp(file[i], "EA", 2) == 0
-			|| ft_memcmp(file[i], "F", 1) == 0
-			|| ft_memcmp(file[i], "C", 1) == 0
-			|| file[i][0] == '\n')
-			last = i;
+		if (is_top_of_map(file[i]) == true)
+			return (i);
 	}
-	i = last + 1;
-	while (file[i] && file[i][0] == '\0')
-		i++;
-	return (i);
+	return (-1);
 }
 
 bool	get_map(t_cub *cub)
 {
-	int	cur_i;
+	int	map_i;
 
-	cur_i = get_cur_i(cub->parse.file);
-	if (cub->parse.file[cur_i] == NULL)
+	map_i = get_map_i(cub->parse.file);
+	if (map_i == -1)
 		return (err_msg(ERR_MAP_NF), false);
-	cub->parse.map = get_2darr_cp(&cub->parse.file[cur_i]);
+	cub->parse.map = get_2darr_cp(&cub->parse.file[map_i]);
 	cub->parse.max_map_y = get_arr_size(cub->parse.map);
 	return (true);
 }
